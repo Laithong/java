c447d06506b6110cc6e5da5fd19a05d4815edeec
Laithong <thonglai768@gmail.com>
2019-03-16 02:54:01 +0700

    first commit

---
+++ MyClass.java
@@ -0,0 +1,7 @@
+public class MyClass {
+  public static void main(String[] args) {
+    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int len = txt.length();
+    System.out.println("The length of the txt string is: " + len);
+  }
+}
