67046ba552bec525d3fc77301ed258f99d4cb33a
Laithong <thonglai768@gmail.com>
2019-03-16 03:10:10 +0700

    MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,5 @@
 public class MyClass {
   public static void main(String[] args) {
-    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int len = txt.length();
-    System.out.println("The length of the txt string is: " + len);
+    System.out.println("Hello World");
   }
 }
