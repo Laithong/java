850c1238808b6d2087ac71f21f9f5e140872ed3b
Laithong <thonglai768@gmail.com>
2019-03-16 14:11:57 +0700

    remove MyClass.java from the repo

--- MyClass.java
+++
@@ -1,7 +0,0 @@
-public class VariablesName {
-  public static void main(String[] args) {
-    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int len = txt.length();
-    System.out.println("The length of the txt string is: " + len);
-  }
-}
