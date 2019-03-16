3b99ff279c2f5e70dfb631b27e144e3350e25489
Laithong <thonglai768@gmail.com>
2019-03-16 15:01:41 +0700

    one

---
+++ MyClass.java
@@ -0,0 +1,7 @@
+public class VariablesName {
+  public static void main(String[] args) {
+    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int len = txt.length();
+    System.out.println("The length of the txt string is: " + len);
+  }
+}
